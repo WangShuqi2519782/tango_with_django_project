c3ce91a0a33f5399ae0fd2fda66255d53375aac4
Shuqi Wang <2519782w@student.gla.ac.uk>
2021-07-27 17:53:38 +0800

    chapter 4: addstatic/images

--- rango/views.py
+++ rango/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    html="<html> <body><a href='/rango/about'>About</a></br><p>Rango says hey there partner!</p></body></html>"
-    return HttpResponse(html)
+    #chapter 3:
+    # html="<html> <body><a href='/rango/about/'>About</a></br><p>Rango says hey there partner!</p></body></html>"
+    # return HttpResponse(html)
+    #chapter 4:
+    context_dict={'boldmessage':'Crunchy,creamy,cookie,candy,cupcake!'}
+    return render(request,'rango/index.html',context=context_dict)
 
 def about(request):
     html="<html> <body><a href='/rango/'>Index</a></br><p>Rango says here is the about page.</p></body></html>"
